c6360618ba8eedabc134ae9d58aa935de9777259
Ashwin-Rajesh <40654785+Ashwin-Rajesh@users.noreply.github.com>
2019-11-19 21:42:04 +0530

    Used new RoomType class
    Yet to implement book, and other functionalities

--- Hotels.java
+++ Hotels.java
@@ -5,21 +5,31 @@ public class Hotels
 	String HotelName
 	int StarRating
 	private ArrayList<Room> RoomList;
+	private RoomType[] RoomArray;
 	/* you will need  to go through the array of room lists */
 	/* get rates */
-
-	public Hotels(String HotelName, int StarRating)
+	
+	public Hotels(RoomTypes[] types, String HotelName, int StarRating)
 	{
-		this.HotelName = HotelName;
+		this.HotelName  = HotelName;
 		this.StarRating = StarRating;
-	}
-	public AddRoom(String RoomName, int NumberOfRooms)
-	{
-		Room RoomName = new Room(RoomName,NumberOfRooms)
-
-	}
-	public Rates(LocalDate Day, String RoomName)
+		RoomList = new ArrayList<Room>(rooms);
+		RoomArray = types;
+		for(RoomType r:types)
 		{
-		/*This will give the rates for a certain day and room*/
+			Room[] rooms = r.getRooms();
+			for(Room rm:rooms)
+				RoomList.add(rm);
+		}
 	}
- 	public
+	
+	public Roomtypes[] GetStatus()
+	{
+		return RoomArray;
+	}	
+	
+	public void book(String room_id)
+	{
+		// Search through RoomList and update it too book it, also update NumberOfRooms.
+	}	
+}
