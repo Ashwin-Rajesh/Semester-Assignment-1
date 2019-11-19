a34d043b320c9a8a000de3785ee06d651114e7b3
SuryaSnG <47815168+SuryaSnG@users.noreply.github.com>
2019-11-19 20:19:17 +0000

    Update Hotels.java

--- Hotels.java
+++ Hotels.java
@@ -2,34 +2,39 @@ import java.util.ArrayList;
 import java.time.LocalDate;
 public class Hotels
 {
-	String HotelName
-	int StarRating
-	private ArrayList<Room> RoomList;
-	private RoomType[] RoomArray;
-	/* you will need  to go through the array of room lists */
-	/* get rates */
-	
-	public Hotels(RoomTypes[] types, String HotelName, int StarRating)
-	{
-		this.HotelName  = HotelName;
-		this.StarRating = StarRating;
-		RoomList = new ArrayList<Room>(rooms);
-		RoomArray = types;
-		for(RoomType r:types)
-		{
-			Room[] rooms = r.getRooms();
-			for(Room rm:rooms)
-				RoomList.add(rm);
-		}
-	}
-	
-	public Roomtypes[] GetStatus()
-	{
-		return RoomArray;
-	}	
-	
-	public void book(String room_id)
-	{
-		// Search through RoomList and update it too book it, also update NumberOfRooms.
-	}	
+    String hotelName;
+    int starRating;//3*,4*,5*
+    private ArrayList<Room> roomList;//list of ?
+    private RoomType[] roomArray;//array of objects of RoomType
+    /* you will need  to go through the array of room lists */
+    /* get rates */
+    public Hotels(RoomType[] types, String hotelName, int starRating)
+    {
+        this.hotelName = hotelName;
+        this.starRating = starRating;
+        roomList = new ArrayList<Room>(rooms);
+        roomArray = types;
+        for(RoomType r : types)
+        {
+            Room[] rooms = r.getRooms();
+            for(Room rm : rooms)
+            {
+                roomList.add(rm);
+            }
+        }
+    }
+    public RoomType[] GetStatus()
+    {
+        return roomArray;
+    }
+    public void book(String room_id)
+    {
+        for(Room r1 : roomList)
+        {
+            if(room_id==r1.getRoomID())
+            {
+                r1.setStatus("NOT AVAILABLE");
+            }
+        }
+    }   
 }
