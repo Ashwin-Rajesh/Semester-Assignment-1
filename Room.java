31ddcc8d9d2f0f8884e1742f4894b73139a5f9f1
Ashwin-Rajesh <40654785+Ashwin-Rajesh@users.noreply.github.com>
2019-11-23 22:46:55 +0530

    implemented isAvailableOn()

--- Room.java
+++ Room.java
@@ -56,5 +56,11 @@ public class Room
     {
         breakfast = flag;
     }
-    
+    public boolean isAvailableOn(LocalDate dt, int period)
+    {
+        for(Reservation r : details)
+            if(!r.isAvailableOn(dt,period))
+                return 0;
+        return 1;
+    }    
 }
