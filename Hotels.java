5da8e9ece46cbc2f887e3b95c43be454655cd20c
SuryaSnG <47815168+SuryaSnG@users.noreply.github.com>
2019-11-23 22:13:19 +0530

    Update Hotels.java

--- Hotels.java
+++ Hotels.java
@@ -3,15 +3,13 @@ import java.time.LocalDate;
 public class Hotels
 {
     String hotelName;
-    int starRating;//3*,4*,5*
     private ArrayList<Room> listOfRooms;//list of rooms
     private ArrayList<RoomType> listOfTypes;//array of objects of RoomType
     /* you will need  to go through the array of room lists */
     /* get rates */
-    public Hotels(ArrayList<RoomType> types, String hotelName, int starRating)
+    public Hotels(ArrayList<RoomType> types, String hotelName)
     {
         this.hotelName = hotelName;
-        this.starRating = starRating;
         listOfRooms = new ArrayList<Room>();
         listOfTypes = types;
         for(RoomType r : types)
